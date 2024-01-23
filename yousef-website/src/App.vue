c2693a336f3214d84c83a9a03dc8f2e1688ed908
yousef <ym222cx@student.lnu.se>
2024-01-23 01:59:17 +0100

    lightwight start with the website

--- yousef-website/src/App.vue
+++ yousef-website/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePage msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from "./components/HomePage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HomePage: HomePage,
+  },
+};
 </script>
 
 <style>
@@ -20,7 +19,15 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  /*margin-top: 60px;*/
+}
+body {
+  background-color: #080808;
 }
 </style>
+
+/*
+https://www.youtube.com/watch?v=13fv-xEAFmY&list=PLDoPjvoNmBAxr5AqK3Yz4DWYKVSmIFziw  
+this is a youtube chanle 
+*/
---
+++ yousef-website/src/components/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hello">helllowwwww</div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
